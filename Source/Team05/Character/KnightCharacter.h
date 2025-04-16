978ce5e283ba2f1b4758c37659d07b915404b23a
eomcri <jihun.eomcri@gmail.com>
2025-04-16 20:58:04 +0900

    [지훈][수정] 주석 변경

--- Source/Team05/Character/KnightCharacter.h
+++ Source/Team05/Character/KnightCharacter.h
@@ -1,11 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+//KnightCharacter.h
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "BaseCharacter.h"
 #include "Ability/SpecialAttack.h"
-#include "UObject/Object.h"
 #include "KnightCharacter.generated.h"
 
 UCLASS()
